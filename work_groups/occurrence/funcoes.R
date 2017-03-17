4e1e4755382d030c5b3714f8cdcdb992ee1973e0
pablopains <pablopains@yahoo.com.br>
2017-03-17 09:27:18 -0300

    Baixar, Juntar e Limpar Ocorrências
    Funções e rotinas para checar nomes e sinônimos, baixar, juntar e limpar
    ocorrências

---
+++ work_groups/occurrence/funcoes.R
@@ -0,0 +1,199 @@
+library('taxize')
+library('spocc')
+library('flora')
+library('divagis')
+library("ggmap")
+library('mapr')
+library('sp')
+library('rgdal')
+library('maptools')
+
+library('dplyr') ## para manipular BD
+library('rgbif')
+
+# ajustar conforme fonte do pacote flora
+load('D:\\R\\flora\\flora-master\\R\\sysdata.rda')
+
+
+### Busca Ocorrências na base gbif
+####################################################################################=c("name","decimalLongitude","decimalLatitude", "collectionCode","catalogNumber","country","stateProvince","county","locality","day","month","year","recordedBy","recordNumber","occurrenceRemarks")
+#################################################################################### i=2
+busca_ocorrencia_gbif <- function(sp_search, name_fields=c("issues","scientificName","decimalLongitude","decimalLatitude", "collectionCode","catalogNumber","identifiedBy","country","stateProvince","county","locality","day","month","year","recordedBy","recordNumber","occurrenceRemarks"), remove.badissues=TRUE, limite=0){
+  all_occ <- {}
+  for (i in 1:NROW(sp_search)){
+    sp.name <- as.character(sp_search$search.str[i])
+    speciesKey <- name_backbone(name=sp.name)$speciesKey
+    if (limite==0){
+      if (length(speciesKey)>0){n.occ <- occ_count(speciesKey,basisOfRecord = 'PRESERVED_SPECIMEN')}
+      else{n.occ <- 10}}
+    else{n.occ <-limite}
+    
+    cat(' -> ',sp.name,' [key gbif ',speciesKey,'(',n.occ,') ]')
+    
+    ### busca via spocc
+    # c("name","longitude","latitude", "collectionCode","catalogNumber","identifiedBy","country","stateProvince","county","locality","day","month","year","recordedBy","recordNumber","occurrenceRemarks","issues")
+      # dat.spocc <- occ(query = sp.name, from = source.data,limit = n.occ)
+    #    if(NROW(dat.spocc$gbif$data[[1]])==0){cat(' (Sem Registros)')}
+    #    else{
+    #      dat <- as.data.frame(dat.spocc$gbif$data[[1]])
+    ###
+    
+    # busca registros 
+    dat.full <- occ_search( scientificName = sp.name, limit = n.occ, basisOfRecord = 'PRESERVED_SPECIMEN')
+    #dat.full <- dat.full[!duplicated(dat.full),] 
+    cat(' - (',NROW(dat.full$data),') encontrados ')
+
+    # retira issues restritivos
+    if (remove.badissues & NROW(dat.full$data) > 0){
+      dat.full.in <- trata_issues(dat.full) 
+      cat(' - (',NROW(dat.full.in$data),') sem issues ')}
+    else{dat.full.in <-dat.full}
+    
+    dat <- {}
+    if(NROW(dat.full.in$data)==0){cat(' (Sem Registros)')}
+    else{
+      dat <- as.data.frame(dat.full.in$data)
+      colretorno <- name_fields[name_fields %in% colnames(dat)]
+      if(length(colretorno)==length(name_fields)){
+        dat = as.data.frame(dat[,name_fields])
+        colnames(dat) = name_fields
+        all_occ <- rbind2(all_occ,dat)
+        cat(' - (',NROW(dat),') baixadas ')}
+      else{
+        if(NROW(dat)>1){cat(' # ',sp.name,'( Colunas Diferentes: ',name_fields[!name_fields %in% colnames(dat)],')')}}}}  
+return(all_occ)}
+###################################################################################
+
+###################################################################################
+### Busca subespécies, variedades e sinônimos coforme flora BR
+### taxon_rank_search = c('species','subspecies','variety')
+### return_type  = 'names','names_synonyms','synonyms'
+###################################################################################
+nomes_sinonimos_florabr <- function(sp_name_search, taxon_rank_search=c('species','subspecies','variety'), return_type='names'){
+
+  names_florabr <- all.taxa[which(all.taxa$search.str==sp_name_search & all.taxa$taxon.rank %in% taxon_rank_search),] 
+  
+  ### lista as subespécies e variedades de uma espécie
+  #spname <-  get.taxa(sp_name_search)$search.str
+  #genus_search <- substr(spname,1, gregexpr(' ', spname)[[1]][1]-1)
+  #specific.epiteth_search <- substr(spname, gregexpr(' ', spname)[[1]][1]+1, nchar(spname))
+  #names_florabr <- all.taxa[which(all.taxa$genus== genus_search & all.taxa$specific.epiteth == specific.epiteth_search & all.taxa$taxon.rank %in% taxon_rank_search),]
+  ###
+  
+  synonyms_florabr <- all.taxa$search.str[all.taxa$id %in% relationships$related.id[relationships$id %in% names_florabr$id]]
+  names <- distinct(as.data.frame(names_florabr,stringsAsFactors = FALSE),search.str)
+  synanyms <- distinct(as.data.frame(synonyms_florabr,stringsAsFactors = FALSE))
+  colnames(names) <- colnames(synanyms)<- c('search.str')
+
+  if (return_type == 'names'){return(names)}
+  if (return_type == 'synonyms'){return(synanyms)}
+  if (return_type == 'names_synonyms'){return(rbind(names,synanyms))}
+}
+##################################################################################
+nomes_sinonimos_tropicos <- function(sp.name,return_type='names_synonyms'){
+  #require('taxize')
+  x <- synonyms(c(sp.name),db="tropicos")[[1]]
+  synanyms <- as.data.frame(distinct(as.data.frame(x$scientificname)),stringsAsFactors = FALSE)
+  names <- as.data.frame(tp_search(name = sp.name)$scientificname,stringsAsFactors = FALSE)
+  colnames(names) <- c('search.str')
+  colnames(synanyms) <- c('search.str')
+  if (return_type == 'names'){return(names)}
+  if (return_type == 'synonyms'){return(synanyms)}
+  if (return_type == 'names_synonyms'){return(rbind(names,synanyms))}
+}
+##################################################################################
+##################################################################################
+# Trata issues
+##################################################################################
+trata_issues <- function(dat.full){
+  dat.full <- dat.full %>% occ_issues(mutate = "expand")
+
+  # ver detalhes em http://gbif.github.io/gbif-api/apidocs/org/gbif/api/vocabulary/OccurrenceIssue.html
+  dat.full.in <- dat.full %>% occ_issues(-ZERO_COORDINATE,
+                                         -COORDINATE_OUT_OF_RANGE,
+                                         -COORDINATE_INVALID,
+                                         -GEODETIC_DATUM_INVALID,
+                                         -COORDINATE_REPROJECTION_FAILED,
+                                         -COORDINATE_ACCURACY_INVALID,
+                                         -COORDINATE_PRECISION_INVALID,
+                                         -COORDINATE_UNCERTAINTY_METERS_INVALID,
+                                         -COUNTRY_INVALID,
+                                         -CONTINENT_INVALID,
+                                         -PRESUMED_SWAPPED_COORDINATE
+                                         -RECORDED_DATE_INVALID,
+                                         -PRESUMED_NEGATED_LONGITUDE,
+                                         -PRESUMED_NEGATED_LATITUDE,
+                                         -BASIS_OF_RECORD_INVALID)
+return(dat.full.in)}
+
+##################################################################################
+#Salva dados conforme processo
+##################################################################################
+#base = gbif, splink, jabot, literatura
+#dir = allrecords, clean, 
+##################################################################################
+save_occurrence_records <- function(dat,file.name,base='gbif',dir='allrecords'){
+  ## Grava resultados totais da busca por registros
+  wd.root <- getwd()
+  if( ! dir.exists(paste0(getwd(),"/occurrencerecords/")))
+  {dir.create("OccurrenceRecords")}
+  setwd(paste0(getwd(),"/occurrencerecords"))
+  if(! dir.exists(paste0(getwd(),"/",base,"/")))
+  {dir.create(base)}
+  setwd(paste0(getwd(),"/",base))
+  if( ! dir.exists(paste0(getwd(),"/",dir,"/")))
+  {dir.create(dir)}
+  setwd(paste0(getwd(),"/",dir))
+  file_name_txt <- paste0(file.name,".txt")
+  write.table(dat,file_name_txt,append = FALSE, quote = TRUE, sep = " ",eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = TRUE, qmethod = "double", fileEncoding = "UTF-8")
+  setwd(wd.root)
+}
+##################################################################################
+#base = gbif, splink, jabot, literatura
+#sep = '\t', ';'
+#encoding = "UTF-8", Latin-1
+##################################################################################
+##################################################################################
+read_occurrence_records <- function(file.name,base='splink',dir='allrecords',sep='\t',encoding="UTF-8",name.fields=''){
+
+  colunas.splink <- c("scientificname","longitude","latitude", "collectioncode","catalognumber","identifiedby","country","stateprovince","county","locality","daycollected","monthcollected","yearcollected","collector","collectornumber","notes")	
+  colunas.splink_mun <- c("scientificname","longitude_mun","latitude_mun", "collectioncode","catalognumber","identifiedby","country","stateprovince","county","locality","daycollected","monthcollected","yearcollected","collector","collectornumber","notes")	
+  colunas.jabot <- c("taxoncompleto","longitude","latitude", "siglacolecao","numtombo","determinador","pais","estado_prov","cidade","descrlocal","diacoleta","mescoleta","anocoleta","coletor","numcoleta","notas")	
+  colunas.gbif <- c("scientificName","decimalLongitude","decimalLatitude", "collectionCode","catalogNumber","identifiedBy","country","stateProvince","county","locality","day","month","year","recordedBy","recordNumber","occurrenceRemarks")	
+
+file.csv <- paste0(getwd(),"/occurrencerecords/",base,'/',dir,'/',file.name,".txt")
+cat('lendo: ',file.csv)  
+if(file.exists(file.csv)){
+  occ.csv.full = read.csv(file.csv, dec='.',sep=sep, header = TRUE,encoding= encoding )
+  if(NROW(occ.csv.full)>0){
+    if(base=='gbif')  {name.fields=colunas.gbif}
+    if(base=='splink'){name.fields=colunas.splink}
+    if(base=='jabot') {name.fields=colunas.jabot} #& length(name.fields)==0
+    if(base=='all'){
+      name.fields=c(colunas.splink,'source')
+      occ.csv <- {}
+      occ.csv = occ.csv.full[,name.fields]
+      cat(' (',base,': ',NROW(occ.csv),' carregados)')  
+      colnames(occ.csv) <- name.fields
+      return(as.data.frame(occ.csv, stringsAsFactors = FALSE))
+    }
+    occ.csv <- {}
+    occ.csv = occ.csv.full[,name.fields]
+    occ.csv <- cbind(occ.csv,c(rep(base,NROW(occ.csv))))
+    colnames(occ.csv) = c(colunas.splink,'source')
+    cat(' (',base,': ',NROW(occ.csv),' carregados)')  
+
+    if(base=='splink'){
+      occ.csv_mun = {}
+      occ.csv_mun = occ.csv.full[,colunas.splink_mun]
+      occ.csv_mun <- occ.csv_mun[!occ.csv_mun$latitude_mun %in% NA & !occ.csv_mun$longitude_mun %in% NA,]  
+      occ.csv_mun <- cbind(occ.csv_mun,c(rep('splink.mun',NROW(occ.csv_mun))))
+      colnames(occ.csv_mun) <- c(colunas.splink,'source')
+      occ.csv <- rbind(occ.csv,occ.csv_mun)
+      cat('( splinl.cm: ',NROW(occ.csv_mun),' carregados)')  
+      }
+    }
+    return(as.data.frame(occ.csv,stringsAsFactors = FALSE))  
+}
+else{cat(file.csv,' não encontrado')}
+}
---
+++ work_groups/occurrence/rotinas.R
@@ -0,0 +1,109 @@
+
+##################################################################################
+
+# pasta do projeto em cada fonde de dados
+setwd("D:\\GitHub\\SDMGroup\\work_groups\\occurrence")
+diretorio <-"plantascalcario" 
+
+### tipos de entrada
+# arquivo com lista de espécies
+spp.csv <- read.table('spp.txt', header=TRUE, sep="\t",dec = ".") 
+spp <- as.matrix(spp.csv[ ,1])
+
+#ou espécie por espécie
+sp <- spp <- c('Cavanillesia umbellata')
+sp <- spp <- c('Aralia warmingiana')
+
+# loop baixa gbig
+for (i in 1:NROW(spp)){
+  sp <- spp[i]
+  cat(' (',i,'-',sp)
+  if(!file.exists(paste0(getwd(),'/OccurrenceRecords/gbif/',diretorio,'/',sp,'.txt'))){
+    spp.search <- nomes_sinonimos_florabr(sp,return_type='names')#names_synonyms
+    if (NROW(spp.search)>0){
+      x<-busca_ocorrencia_gbif(spp.search)
+      if (NROW(x)>0)
+      {save_occurrence_records(x,sp,base = 'gbif',dir = diretorio)}
+      else{cat('=','sem registros!')}
+    }else{cat('=',' não encontrada em floraBR!)')}
+  }else{cat('=','já baixado!)')}
+}
+
+#junção de dados de arquivos de mesmo nome de diferentes bases (por nome de especie por exemplo)
+
+for (i in 1:NROW(spp)){
+  sp <- spp[i]
+  cat(' (',i,'-',sp)
+  x <- xg <- xs <- {}
+  xg <- read_occurrence_records(sp,base='gbif',sep=' ',dir=diretorio,encoding= 'Latin-1')
+  xs <- read_occurrence_records(sp,base='splink',dir=diretorio)
+  xj <- read_occurrence_records(sp,base='jabot',sep=',',dir=diretorio,encoding= 'Latin-1')
+  x <- rbind(xg,xs,xj)
+  save_occurrence_records(x,sp,base = 'all',dir=diretorio)
+}
+
+### Limpeza 
+BR.pol <- readShapePoly(fn=paste0(getwd(),"/GISBase/BR.shp"))
+NT.pol <- readShapePoly(fn=paste0(getwd(),"/GISBase/neotropical.shp"))
+
+### especificar polígono limite
+pol.limite <- NT.pol
+###
+
+### regarrega dados depois da junção
+occ.tmp <- occ.full <- read_occurrence_records(spp,base='all',sep=' ',dir=diretorio,encoding= 'Latin-1')
+cat(' (total. ',NROW(occ.full),')')
+
+# Retira registros com lat/long = 0 e NA
+remove_latlongNA0=TRUE # testar com lastInterpreted !!!
+if(remove_latlongNA0){
+  occ.tmp <- occ.tmp[!occ.tmp$latitude %in% 0 & !occ.tmp$longitude %in% 0 & !occ.tmp$latitude %in% NA & !occ.tmp$longitude %in% NA,]
+  cat(' (Lat.Long NA e 0:',NROW(occ.full)-NROW(occ.tmp),')')}
+
+remove_regdup=FALSE
+if(remove_regdup){
+  occ.tmp.dup <- occ.tmp[duplicated(occ.tmp[1:length(occ.full)-1]),]
+  occ.tmp <- occ.tmp[!duplicated(occ.tmp[]),] #Retira duplicados, fora source
+  cat(' (reg.dup.:',NROW(occ.tmp.dup),')')}
+
+remove_anoNA=TRUE
+if(remove_anoNA){
+  occ.tmp.anoNA <- occ.tmp[occ.tmp$yearcollected %in% NA,]
+  occ.tmp <- occ.tmp[!occ.tmp$yearcollected %in% NA,] # retira anos NA 
+  cat(' (ano NA:',NROW(occ.tmp.anoNA),')')}  
+
+remove_anocorte=1967
+if(remove_anocorte>0){
+  occ.tmp.anocorte <- occ.tmp[occ.tmp$yearcollected >= remove_anocorte,]
+  occ.tmp <- occ.tmp[occ.tmp$yearcollected >= remove_anocorte,] # retira ano < 
+  cat(' (acima ano corte ',remove_anocorte,' ',NROW(occ.tmp.anocorte),')')}  
+
+
+remove_duplicata=FALSE # testar com lastInterpreted !!!
+if(remove_duplicata){
+  occ.tmp.dupcoletor <- occ.tmp[duplicated(occ.tmp$collector,occ.tmp$collectornumber,occ.tmp$yearcollected, occ.tmp$monthcollected, occ.tmp$daycollected ),]
+  occ.tmp <- occ.tmp[!duplicated(occ.tmp$collector,occ.tmp$collectornumber,occ.tmp$yearcollected, occ.tmp$monthcollected, occ.tmp$daycollected ),] # retita coletor repetido
+  cat(' (dup.coletor ',NROW(occ.tmp.dupcoletor),')')
+  
+  occ.tmp.dupcolecao <- occ.tmp[duplicated(occ.tmp$collectioncode,occ.tmp$catalognumber,occ.tmp$yearcollected, occ.tmp$monthcollected, occ.tmp$daycollected ),]
+  occ.tmp <- occ.tmp[!duplicated(occ.tmp$collectioncode,occ.tmp$catalognumber,occ.tmp$yearcollected, occ.tmp$monthcollected, occ.tmp$daycollected ),] # retira colecao e tombo repetido
+  cat(' (dup.coleção ',NROW(occ.tmp.dupcolecao),')')}
+
+remove_limiteespacial=TRUE # testar com lastInterpreted !!!
+if(remove_limiteespacial){
+  occ.tmp.points <- SpatialPointsDataFrame(cbind(occ.tmp$longitude,occ.tmp$latitude),occ.tmp)
+  occ.tmp.points_in <- occ.tmp.points[!is.na(over(occ.tmp.points ,geometry(pol.limite))),] # dentro do poligono
+  occ.tmp.points_out <- occ.tmp.points[is.na(over(occ.tmp.points ,geometry(pol.limite))),] # fora do poligono
+  cat(' (dentro ',NROW(occ.tmp.points_in),') (fora',NROW(occ.tmp.points_out),')')
+}
+
+# mapa exemplo
+plot(pol.limite)
+plot(occ.tmp.points_in, add=TRUE, col="green", pch=19, cex=0.5)
+plot(occ.tmp.points_in, add=TRUE, col="black", pch=19, cex=0.2)
+plot(occ.tmp.points_out, add=TRUE, col="red", pch=19, cex=0.2)
+
+# salva dados da limpeza
+occ.sel <- edit(occ.tmp.points_in@data)
+save_occurrence_records(occ.sel,paste0('Limpeza-',sp),base = 'all',dir=diretorio)
+
