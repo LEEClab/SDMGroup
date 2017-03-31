935d0f127ba83ea9989a3d646337cbdec56790c8
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2017-03-31 10:20:35 -0300

    ensemble

--- meets/meet004/03_script_ensemble.R
+++ meets/meet004/03_script_ensemble.R
@@ -2,7 +2,6 @@
 
 # Thadeu Sobral de Souza - thadeusobral@gmail.com 
 # Maurício Humberto Vancine - mauricio.vancine@gmail.com
-# Cleber Chaves - cleberchaves@gmail.com 
 
 ###-----------------------------------------------------------------------------------------###
 
@@ -115,3 +114,58 @@ for(i in sp){
 	   ens.al[] <- 0}}}
 
 ###-----------------------------------------------------------------------------------------###
+
+## average ensemble 
+
+# lists
+# species
+sp <- list("Aralia_warmingiana", "Cavanillesia_umbellata")
+sp
+
+# gcms
+gc <- list("CCSM")
+gc
+
+# periods
+pe <- list("0k", "6k", "21k")
+pe
+
+# algorithms
+al <- list("Bioclim", "Gower", "Maha", "Maxent", "SVM")
+al
+
+# replicates
+re <- list(1:5)
+re
+# ensembles
+va <- matrix(NA, nrow = ncell(enm), ncol = length(al))
+va
+
+ens.al <- enm[[1]]
+ens.al[] <- NA
+names(ens.al) <- "ens.al"
+ens.al
+
+for(i in sp){		
+  asc.sp <- grep(i, asc, value = T)
+  
+    for(j in gc){		
+      asc.gc <- grep(j, asc.sp, value = T)
+              
+	   for(k in pe){		
+          asc.pe <- grep(k, asc.gc, value = T)
+
+            for(l in al){		
+              asc.al <- grep(l, asc.pe, value = T)
+          	             
+                for(m in re){		
+			enm.al <- stack(asc.al)
+                  va[, m] <- values(enm.al[[m]])}
+			
+		  ens.al[] <- apply(va, 1, mean)
+		  
+		  writeRaster(ens.al, paste0("ensemble_aver_", i, "_", j, "_", k, "_", l, ".asc"), 
+			        format = "ascii")
+
+	   va <- matrix(NA, nrow = ncell(enm), ncol = length(al))
+            		ens.al[] <- NA}}}}
