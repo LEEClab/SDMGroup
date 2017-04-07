8509bfa329f9e998f0d98422121c5e3ac8c00b8b
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2017-04-06 23:22:30 -0300

    enms

---
+++ meets/meet006/02_script_build_enm.R
@@ -0,0 +1,242 @@
+### script build enm ###
+
+# Thadeu Sobral de Souza - thadeusobral@gmail.com 
+# Maurício Humberto Vancine - mauricio.vancine@gmail.com
+
+###-----------------------------------------------------------------------------------------###
+### script build enm ### 
+###-----------------------------------------------------------------------------------------###
+
+# 1. limpara a memoria e carregar os pacotes 
+# limpar o workspace e aumentar a memoria para o r
+rm(list = ls())
+memory.limit(size = 17500000000000) 
+
+# instalar e carregar pacotes
+# install.packages(c("raster", "rgdal", "dismo", "gam", "randomForest", "kernlab", "rJava", 
+# 		   	   "vegan"), dep = T)
+
+# carregar pacotes
+library(raster) # manejo de arquivos sig 
+library(rgdal) # manejo de arquivos sig
+library(dismo) # construir enms
+library(gam) # construir enms
+library(randomForest) # construir enms
+library(kernlab) # algoritmo svm
+library(rJava) # funcionamento do java
+library(vegan) # diversas analises multivariadas
+
+# verificar pacotes carregados
+search()
+
+###-----------------------------------------------------------------------------------------###
+
+# 2. dados de entrada
+# diretorio da pasta de dados de entrada
+setwd("D:/github/SDMGroup/meets/meet006/data")
+
+# importando os pontos de ocorrencia 
+points <- read.table("natalia_birdies.txt", h = T, sep = "\t")
+str(points)
+
+am <- shapefile("D:/github/SDMGroup/meets/meet006/data/south_america_diss_gcs_wgs84.shp")
+am
+
+plot(am, col = "gray80", axes = T, xlim = c(-60, -60), main = "Mapa Natalia Birdies")
+points(points[points$sp == "S_lineola", 2], points[points$sp == "S_lineola", 3], pch = 20, add = T)
+
+# importando as variaveis .asc
+asc <- list.files(patt = "asc")
+asc
+
+asc.0k <- grep("0k", asc, value = T)
+asc.0k
+
+env.stack.0k <- stack(asc.0k)
+names(env.stack.0k) <- paste0("bio", c("01", "02", "03", "16", "17"))
+env.stack.0k
+
+
+plot(env.stack.0k)
+
+plot(env.stack.0k[[1]])
+points(points[, 2], points[, 3], pch = 20, col = points$sp)
+
+
+# extraindo os valores de cada celula e adicionando as coordenadas
+values <- values(env.stack.0k)[, 1]
+head(values, 10)
+
+id <- 1:ncell(env.stack.0k)
+head(id, 10)
+
+coord <- xyFromCell(env.stack.0k, id)
+head(coord, 10)
+
+plot(env.stack.0k[[1]])
+points(coord, pch = "o", cex = 0.1)
+
+coords <- data.frame(coord, values)
+coords
+
+coords <- na.omit(coords)
+coords
+
+coords <- coords[, -3]
+head(coords)
+
+colnames(coords) <- c("long", "lat")
+head(coords, 10)
+
+plot(env.stack.0k[[1]])
+points(coords, pch = "o", cex = 0.1)
+
+###-----------------------------------------------------------------------------------------###
+
+# verificar maxent
+jar <- paste(system.file(package = "dismo"), "/java/maxent.jar", sep = "")
+file.exists(jar)
+
+###-----------------------------------------------------------------------------------------###
+
+
+# ENMs
+
+# diretorio de saida dos enms
+setwd("..")
+getwd()
+dir.create("_saidas_enm")
+setwd("_saidas_enm")
+getwd()
+
+# aogcms
+AOGCM <- "CCSM"
+AOGCM
+
+# enms
+for(i in 1:length(levels(points[, 1]))){ # for para cada especie
+
+eval.Bioclim <- NULL # somente presenca - envelope
+eval.Gower <- NULL # somente presenca - distancia
+eval.Maha <- NULL # somente presenca - distancia
+eval.Maxent <- NULL # presenca/pseudo-ausencia
+eval.SVM <- NULL # presenca/pseudo-ausencia
+
+eval.names <- NULL
+
+# selecionando presença e ausencia da especie
+	id.specie <- levels(points[, 1])[i]
+	pr.specie <- points[which(points[, 1] == id.specie), 2:3]
+	id.background <- sample(nrow(coords), nrow(pr.specie))
+	bc.specie <- coords[id.background, ]
+	
+
+for(r in 1:5){	# numero de replicas
+## preparando os modelos
+# data treino e teste	
+	pr.sample.train <- sample(nrow(pr.specie), round(0.75 * nrow(pr.specie)))
+	bc.sample.train <- sample(nrow(bc.specie), round(0.75 * nrow(bc.specie)))
+	test <- na.omit(prepareData(x = env.stack.0k, p = pr.specie[-pr.sample.train, ], b = bc.specie[-bc.sample.train, ]))
+	train <- na.omit(prepareData(x = env.stack.0k, p = pr.specie[pr.sample.train, ], b = bc.specie[bc.sample.train, ]))
+
+ 
+####### ALGORITMOS
+## Bioclim	
+	Bioclim <- bioclim(train[which(train[, 1] == 1), -1])	
+	
+ writeRaster(predict(env.stack.0k, Bioclim), paste(AOGCM, "_Bioclim_0k_", id.specie, r, ".asc", sep = ""), format = "ascii")	
+
+	eBioclim <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = Bioclim)
+	idBioclim <- which(eBioclim@t == as.numeric(threshold(eBioclim, "spec_sens")))
+	eval.Bioclim.sp <- c(eBioclim@t[idBioclim], eBioclim@auc, (eBioclim@TPR[idBioclim]+eBioclim@TNR[idBioclim]-1))
+	eval.Bioclim <- rbind(eval.Bioclim, eval.Bioclim.sp)
+
+## Gower	
+	Gower <- domain(train[which(train[, 1] == 1), -1])	
+
+ writeRaster(predict(env.stack.0k, Gower), paste(AOGCM, "_Gower_0k_", id.specie, r, ".asc", sep = ""), format = "ascii") 
+ 
+	eGower <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = Gower)
+	idGower <- which(eGower@t == as.numeric(threshold(eGower, "spec_sens")))
+	eval.Gower.sp <- c(eGower@t[idGower], eGower@auc, (eGower@TPR[idGower]+eGower@TNR[idGower]-1))
+	eval.Gower <- rbind(eval.Gower, eval.Gower.sp)
+
+## Maha	
+	Maha <- mahal(train[which(train[, 1] == 1), -1])	
+	
+ writeRaster(predict(env.stack.0k, Maha), paste(AOGCM, "_Maha_0k_", id.specie, r, ".asc", sep = ""), format = "ascii") 
+
+	eMaha <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = Maha)
+	idMaha <- which(eMaha@t == as.numeric(threshold(eMaha, "spec_sens")))
+	eval.Maha.sp <- c(eMaha@t[idMaha], eMaha@auc, (eMaha@TPR[idMaha]+eMaha@TNR[idMaha]-1))
+	eval.Maha <- rbind(eval.Maha, eval.Maha.sp)
+	
+
+## Maxent	
+	Maxent <- maxent(train[, -1], train[, 1])	
+
+ writeRaster(predict(env.stack.0k, Maxent), paste(AOGCM, "_Maxent_0k_", id.specie, r, ".asc", sep = ""), format = "ascii") 
+
+	eMaxent <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = Maxent)
+	idMaxent <- which(eMaxent@t == as.numeric(threshold(eMaxent, "spec_sens")))
+	eval.Maxent.sp <- c(eMaxent@t[idMaxent], eMaxent@auc, (eMaxent@TPR[idMaxent]+eMaxent@TNR[idMaxent]-1))
+	eval.Maxent <- rbind(eval.Maxent, eval.Maxent.sp)
+
+
+## SVM	
+	SVM <- ksvm(pb ~ bio01 + bio02 + bio03 + bio16 + bio17, data = train)	
+
+ writeRaster(predict(env.stack.0k, SVM), paste(AOGCM, "_SVM_0k_", id.specie, r, ".asc", sep = ""), format = "ascii") 
+
+	eSVM <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = SVM)
+	idSVM <- which(eSVM@t == as.numeric(threshold(eSVM, "spec_sens")))
+	eval.SVM.sp <- c(eSVM@t[idSVM], eSVM@auc, (eSVM@TPR[idSVM]+eSVM@TNR[idSVM]-1))
+	eval.SVM <- rbind(eval.SVM, eval.SVM.sp)
+
+
+	eval.names <- c(eval.names, paste(id.specie, r, sep = ""))		
+} # ends for"r"
+
+dimnames(eval.Bioclim) <- list(eval.names, c("thrs", "AUC", "TSS"))
+dimnames(eval.Gower) <- list(eval.names, c("thrs", "AUC", "TSS"))
+dimnames(eval.Maha) <- list(eval.names, c("thrs", "AUC", "TSS"))
+dimnames(eval.Maxent) <- list(eval.names, c("thrs", "AUC", "TSS"))
+dimnames(eval.SVM) <- list(eval.names, c("thrs", "AUC", "TSS"))
+
+
+write.table(eval.Bioclim, paste("zEval_", AOGCM, "_Bioclim_", id.specie, ".txt", sep = ""))
+write.table(eval.Gower, paste("zEval_", AOGCM, "_Gower_", id.specie, ".txt", sep = ""))
+write.table(eval.Maha, paste("zEval_", AOGCM, "_Maha_", id.specie, ".txt", sep = ""))
+write.table(eval.Maxent, paste("zEval_", AOGCM, "_Maxent_", id.specie, ".txt", sep = ""))
+write.table(eval.SVM, paste("zEval_", AOGCM, "_SVM_", id.specie, ".txt", sep = ""))
+
+} # ends for"i"
+
+###-----------------------------------------------------------------------------------------###
+
+
+# plotando as predicoes
+
+# divide o plot em 1 linha e 5 colunas
+par(mfrow = c(2, 3))
+
+# bioclim
+m1 <- raster ("CCSM_Bioclim_0k_B.balansae1.asc")
+plot (m1, main = "bioclim")
+
+# gower
+m2 <- raster ("CCSM_Gower_0k_B.balansae1.asc")
+plot(m2, main = "gower")
+
+# mahanalobis
+m3 <- raster ("CCSM_Maha_0k_B.balansae1.asc")
+plot(m3, main = "mahanalobis")
+
+# maxent
+m4 <- raster("CCSM_Maxent_0k_B.balansae1.asc")
+plot(m4, main = "maxent")
+
+# svm
+m5 <-raster ("CCSM_SVM_0k_B.balansae1.asc")
+plot(m5, main = "svm")
+
---
+++ meets/meet006/03_script_ensemble.R
@@ -0,0 +1,117 @@
+### script ensemble frequency import data sequence ###
+
+# Thadeu Sobral de Souza - thadeusobral@gmail.com 
+# Maurício Humberto Vancine - mauricio.vancine@gmail.com
+
+###-----------------------------------------------------------------------------------------###
+
+# 1. limpar a memoria e carregar os pacotes 
+# limpar o workspace e aumentar a memoria para o r
+rm(list = ls())
+memory.limit(size = 17500000000000) 
+
+# instalar e carregar pacotes
+# install.packages(c("raster", "rgdal", "vegan"), dep = T)
+
+# carregar pacotes
+library(raster) # manejo de arquivos sig 
+library(rgdal) # manejo de arquivos sig
+library(vegan) # diversas analises multivariadas
+
+# verificar pacotes carregados
+search()
+
+
+###-----------------------------------------------------------------------------------------###
+
+# import data
+# directory
+setwd("D:/github/SDMGroup/meets/meet006/_saidas_enm")
+
+# enms
+# list files
+asc <- list.files(pattern = ".asc")
+asc
+
+enm <- raster("CCSM_Bioclim_0k_S_lineola1.asc")
+enm
+plot(enm)
+
+# evaluate
+txt <- list.files(pattern = ".txt")
+txt
+
+eva <- lapply(txt, read.table)
+eva
+names(eva) <- txt
+eva
+
+###-----------------------------------------------------------------------------------------###
+
+## frequency ensemble 
+# lists
+# species
+sp <- list("S_lineola", "T_nigriceps", "T_subalaris")
+sp
+
+# gcms
+gc <- list("CCSM")
+gc
+
+# periods
+pe <- list("0k")
+pe
+
+# algorithms
+al <- list("Bioclim", "Gower", "Maha", "Maxent", "SVM")
+al
+
+# replicates
+re <- list(1:5)
+re
+
+# ensembles
+ens.re <- enm
+ens.re[] <- 0
+names(ens.re) <- "ens.re"
+ens.re
+
+ens.al <- enm
+ens.al[] <- 0
+names(ens.al) <- "ens.al"
+ens.al
+
+# for
+for(i in sp){		
+  asc.sp <- grep(i, asc, value = T)
+  eva.sp <- eva[grep(i, names(eva))]
+  
+    for(j in gc){		
+      asc.gc <- grep(j, asc.sp, value = T)
+      eva.gc <- eva.sp[grep(j, names(eva.sp))]
+       
+	  for(k in pe){		
+          asc.pe <- grep(k, asc.gc, value = T)
+
+            for(l in al){		
+	        asc.al <- grep(l, asc.pe, value = T)
+                eva.al <- eva.gc[grep(l, names(eva.gc))]
+			           
+	          for(m in re){		
+                    enm.al <- stack(asc.al)
+	            ens.re <- sum(ens.re, enm.al[[m]] >= eva.al[[1]][m, 1])}
+		
+		writeRaster(ens.re, paste0("ensemble_freq_", i, "_", j, "_", k, "_", l, ".asc"), 
+				format = "ascii")
+
+	     ens.al <- sum(ens.al, ens.re)
+		  	
+	     ens.re[] <- 0}
+
+	   writeRaster(ens.al, paste0("ensemble_freq_", i, "_", j, "_", k, ".asc"), format = "ascii")
+	   writeRaster(ens.al / (length(al) * max(re[[1]])), paste0("ensemble_freq_", i, "_", j, "_", k, "_bin.asc"), format = "ascii")
+		
+	   ens.al[] <- 0}}}
+
+###-----------------------------------------------------------------------------------------###
+
